6e4b76c9b9c7b102d8b1c812b67c4e0f63b2cb6d
Thomas Beirne <thomas.beirne@gmail.com>
2018-07-08 00:28:32 -0700

    Fix bad line wrapping

--- assets/app/src/components/text-section.js
+++ assets/app/src/components/text-section.js
@@ -1,8 +1,14 @@
 // @flow
 import * as React from 'react'
+import styled from 'react-emotion'
 
 import Spacer from './spacer'
 import H2 from './h2'
+
+const Pre = styled.pre`
+  white-space: pre-wrap;
+  word-wrap: break-word;
+`
 
 type Props = {
   title: string,
@@ -14,7 +20,7 @@ export default function TextSection({ title, children, pre = false }: Props) {
     <div>
       <H2>{title}</H2>
       <Spacer height={15} />
-      {pre ? <pre>{children}</pre> : <p>{children}</p>}
+      {pre ? <Pre>{children}</Pre> : <p>{children}</p>}
     </div>
   )
 }

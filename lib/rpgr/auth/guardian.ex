86259f4a0d0279c08243dd007c7a810348da2c0b
Thomas Beirne <thomas@buildingconnected.com>
2018-09-25 20:41:21 -0700

    Make more logging

--- lib/rpgr/auth/guardian.ex
+++ lib/rpgr/auth/guardian.ex
@@ -1,15 +1,20 @@
 defmodule Rpgr.Auth.Guardian do
   use Guardian, otp_app: :rpgr
   alias Rpgr.Auth
+  require Logger
 
   def subject_for_token(user, _claims) do
     {:ok, to_string(user.id)}
   end
 
   def resource_from_claims(claims) do
+    Logger.debug("Getting claims")
+
     user =
       claims["sub"]
       |> Auth.get_user!()
+
+    Logger.debug("Got claims")
 
     {:ok, user}
     # If something goes wrong here return {:error, reason}
